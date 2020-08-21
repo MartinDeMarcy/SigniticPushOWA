1af3fc541e25e6d86bb0454c3c49a8e141339a90
MartinDeMarcy <martin.pras@epitech.eu>
2020-08-21 14:55:31 +0200

    init

---
+++ ConsoleApp1/OWA.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.Exchange.WebServices.Data;
+using Microsoft.Identity.Client;
+using System.IO;
+
+namespace OWAUtility
+{
+    public class OWA
+    {
+        public void Start(string[] args)
+        {
+
+            MainAsync(args).Wait();
+
+        }
+
+        static void SetSigniture(ExchangeService service, string path)
+        {
+            service.TraceEnabled = true;
+            Folder Root = Folder.Bind(service, WellKnownFolderName.Root);
+            UserConfiguration OWAConfig = UserConfiguration.Bind(service, "OWA.UserOptions", Root.ParentFolderId, UserConfigurationProperties.All);
+
+            // Open the stream and read it back.
+            string signature = File.ReadAllText(path);
+
+            if (OWAConfig.Dictionary.ContainsKey("signaturehtml"))
+            {
+                OWAConfig.Dictionary["signaturehtml"] = signature;
+            }
+            else
+            {
+                OWAConfig.Dictionary.Add("signaturehtml", signature);
+            }
+           
+            OWAConfig.Update();
+        }
+
+        public static async System.Threading.Tasks.Task MainAsync(string[] args)
+        {
+            // Configure the MSAL client to get tokens
+            var ewsScopes = new string[] { "https://outlook.office.com/.default" };
+
+            var app = ConfidentialClientApplicationBuilder.Create(ConfigurationManager.AppSettings["appId"])
+                .WithAuthority(AzureCloudInstance.AzurePublic, ConfigurationManager.AppSettings["tenantId"])
+                .WithClientSecret(ConfigurationManager.AppSettings["clientSecret"])
+                .Build();
+
+            AuthenticationResult result = null;
+
+            try
+            {
+                // Make the interactive token request
+                result = await app.AcquireTokenForClient(ewsScopes)
+                    .ExecuteAsync();
+
+                // Configure the ExchangeService with the access token
+                var ewsClient = new ExchangeService();
+                ewsClient.Url = new Uri("https://outlook.office365.com/EWS/Exchange.asmx");
+                ewsClient.Credentials = new OAuthCredentials(result.AccessToken);
+
+                //Impersonate the mailbox you'd like to access.
+                ewsClient.ImpersonatedUserId = new ImpersonatedUserId(ConnectingIdType.SmtpAddress, args[0]);
+                SetSigniture(ewsClient, args[1]);
+
+            }
+            catch (MsalException ex)
+            {
+                Console.WriteLine($"Error acquiring access token: {ex.ToString()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.ToString()}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OWAUtility;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // The Length property provides the number of array elements.
+        Console.WriteLine($"parameter count = {args.Length}");
+        OWA owa = new OWA();
+        owa.Start(args);
+
+        if (System.Diagnostics.Debugger.IsAttached)
+        {
+            Console.WriteLine("Hit any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
